657ef40eec7514a470030ddc63d3dfa6c104f883
Shannon Cody Russell <48037883+uncoded0123@users.noreply.github.com>
2024-05-03 05:14:59 -0500

    Update main.py
    now it can be turned on and off remotely by pressing a button connected to esp32

--- controller/main.py
+++ controller/main.py
@@ -1,2 +1,24 @@
-from esp32_now_send import Send
-Send('<enter esp32 receiver board mac address here>').send('70')
+from esp32_now_send import Send  
+import machine
+from time import sleep
+
+
+sender = Send('<enter esp32 receiver board mac address here>')
+button_pin = machine.Pin(15, machine.Pin.IN)
+led_pin = machine.Pin(2, machine.Pin.OUT)
+led_state = False
+
+while True:
+    sleep(0.3)
+    button_state = button_pin.value()
+    if button_state == 0:
+        led_state = not led_state
+        led_pin.value(led_state)
+        if led_state:
+            print('On')
+            sender.send('65')
+            sleep(5)
+        else:
+            print("Off")
+            sender.send('100')
+            sleep(5)
